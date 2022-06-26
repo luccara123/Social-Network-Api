c176d5c9f77950a854ac91717a676081a98722d2
Lucca Araujo <lucca.ara7@gmail.com>
2022-06-25 18:06:51 -0700

    added packages and added users model

---
+++ models/User.js
@@ -0,0 +1,38 @@
+// Calling mongoose, schema
+const { Schema, model } = require("mongoose");
+
+const usersSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Email address is required!'],
+        unique: true,
+        match: [  // validation
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            "A valid email address is required!",
+          ],
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+const User = model('User', usersSchema);
+
+module.exports = User;
